8df0791ab487b0504e6db8c5381463543a0e19e3
Ingar <ingar@megarden.no>
2022-02-18 23:40:49 +0100

    generic flask

---
+++ flask_server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/api/<uuid>', methods=['GET', 'POST'])
+def update(uuid):
+    content = request.json
+    print(content)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1234, debug=True)
