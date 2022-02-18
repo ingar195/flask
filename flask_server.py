693ebaefd6b8e9d38a8b5177c29972689be9bd1c
Ingar <ingar@megarden.no>
2022-02-18 23:41:32 +0100

    added return

--- flask_server.py
+++ flask_server.py
@@ -8,6 +8,8 @@ def update(uuid):
     content = request.json
     print(content)
 
+    return jsonify({'data': 'some json'})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1234, debug=True)
